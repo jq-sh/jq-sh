987a1ffb051604f3025da55b7c51d6eb987387d5
Chris Ottrey <cottrey@zendesk.com>
2021-11-12 22:57:26 +1100

    Refactors

--- shpecs/shpec_helper.sh
+++ shpecs/shpec_helper.sh
@@ -1,12 +1,6 @@
 export PATH=./bin:$PATH
 export PATH="./shpecs/support:$PATH"
 
-color_strip() {
-  sed -e 's/[\[0-9;]*m//g' \
-      -e 's/\[K//g'        \
-      -e 's/ $//g'
-}
-
 input_file=${input_file:-"/dev/null"}
 
 matches_expected() { local cmd="${cmd:-$1}"

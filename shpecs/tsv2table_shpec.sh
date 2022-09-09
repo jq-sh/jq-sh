e2727f36d0c746c91a5939e09fd8efe6d6840410
Chris Ottrey <cottrey@zendesk.com>
2022-09-09 21:40:44 +1000

    test: unit test for $max_width on tsv2table

--- shpecs/tsv2table_shpec.sh
+++ shpecs/tsv2table_shpec.sh
@@ -17,7 +17,7 @@ describe "tsv2table"
 └─────┴───────────┴─────────────┘
 EOF
 
-  describe "title=title1 tsv2table"
+  describe 'with $title'
     matches_expected 'title=title1 tsv2table' \
 <<-EOF
 title1
@@ -30,4 +30,19 @@ title1
 │     │           │tr4.3        │
 └─────┴───────────┴─────────────┘
 EOF
+  end
+
+  describe 'with $max_width'
+    matches_expected 'max_width=30 tsv2table' \
+<<-EOF
+┌─────┬─────────┬─────────────┐
+│th1  │th2      │tableheading3│
+├─────┼─────────┼─────────────┤
+│tr1.1│         │tr1.3        │
+│tr2.1│tabl....2│tablerow2.3  │
+│     │         │             │
+│     │         │tr4.3        │
+└─────┴─────────┴─────────────┘
+EOF
+  end
 end
